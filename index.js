0e43d3759974b083fc6a9d190706801d882568eb
Jake Luer <jake@alogicalparadox.com>
2014-01-01 00:47:05 -0500

    reorganize

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 
 exports.Chan = require('./lib/chan').Chan;
 exports.Port = require('./lib/port').Port;
-exports.DuplexStream = require('./lib/duplexstream').DuplexStream;
+exports.DuplexStream = require('./lib/duplex').DuplexStream;
