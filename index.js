98d3b379501437f22d3b78b634b24b33760269a5
Jake Luer <jake@alogicalparadox.com>
2014-01-02 05:46:54 -0500

    sharedport: initial implementation and tests

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ exports.SharedChan = require('./lib/chan.shared').SharedChan;
 
 // ports
 exports.Port = require('./lib/port').Port;
+exports.SharedPort = require('./lib/port.shared').SharedPort;
 
 // sugar
 exports.Duplex = require('./lib/duplex').Duplex;
