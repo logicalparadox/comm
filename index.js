d9c9940fc74ad07483abf60f5c916c358f0ca35d
Jake Luer <jake@alogicalparadox.com>
2014-01-05 05:59:26 -0500

    remove old Shared*

--- index.js
+++ index.js
@@ -5,11 +5,9 @@ exports.DistributedQueue = require('./lib/queue.distributed').DistributedQueue;
 
 // channels
 exports.Chan = require('./lib/chan').Chan;
-exports.SharedChan = require('./lib/chan.shared').SharedChan;
 
 // ports
 exports.Port = require('./lib/port').Port;
-exports.SharedPort = require('./lib/port.shared').SharedPort;
 
 // sugar
 exports.Duplex = require('./lib/duplex').Duplex;
